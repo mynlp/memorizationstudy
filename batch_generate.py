4e16ea1369f3d98c89cf49b0bb9f179edc253533
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-01-16 16:13:38 +0900

    fix bug

--- batch_generate.py
+++ batch_generate.py
@@ -1,25 +1,17 @@
-#!/usr/bin/env python3
-# coding=utf-8
 import os
 import logging
 import time
 import datetime
 import torch
-import copy
-import numpy as np
 import torch.distributed as dist
-from torch.utils.data import Dataset, DataLoader
-from megatron.data.data_utils import build_the_dataset
-from transformers import AutoModelForCausalLM
 import transformers.utils as transformer_utils
 import multiprocessing as mp
-from tqdm import trange
 from pythia.utils.mmap_dataset import MMapIndexedDataset
 from transformers import GPTNeoXForCausalLM
 
 
 def generate_dataset(batch_size, start_seq_idx, end_seq_idx, mp_queue, prefetch_max=128):
-    prefix = '/scratch/pile/standard/document.bin'
+    prefix = 'undeduped_merge/document.bin'
     if "deduped" in os.environ['MODEL']:
         prefix = 'orz/pile/deduped/document.bin'
     buff_size = 2049*batch_size*2
---
+++ run_generate.sh
@@ -0,0 +1,6 @@
+export RANK=1
+export MODEL=70m-deduped-v0
+
+
+
+python3 generate.py
