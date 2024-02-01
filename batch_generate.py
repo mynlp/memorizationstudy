c08e3b95408ede5837dc2642d5bb2cb515c35604
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-02-01 17:03:51 +0900

    fix dir

--- batch_generate.py
+++ batch_generate.py
@@ -139,7 +139,7 @@ def main():
                 debug_count += 1
                 if debug_count == 200:
                     with open(f"memorization_evals_{MODEL}_{CHECKPOINT}.csv", "w") as f:
-                        f.write("\n".join(memorization_evals))
+                        f.write("\n".join(memorization_evals).encode())
             print(f"Generation uptil {idx} took {time.time() - t:.3}s")
             #dist.barrier()
             iters += 1
