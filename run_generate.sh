be4fad7317c22fd9b5e7e73822202f4f5be817da
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-02-21 16:34:25 +0900

    fix

--- run_generate.sh
+++ run_generate.sh
@@ -16,4 +16,6 @@
 if [ -z "$RUN_ON_REMOTE" ]; then
     source /work/gk77/k77025/.zshrc
 fi
+
+## available models:
 python3 batch_generate.py --model 13m-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 32
