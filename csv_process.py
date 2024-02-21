18cbdc975ebd3374b0e225a718cc200c5a8f0764
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-02-21 17:31:01 +0900

    fix

--- csv_process.py
+++ csv_process.py
@@ -9,10 +9,11 @@ results_list = []
 #results = generate_results[generate_results['0.0'] == 0]
 #results_list.append(results)
 #results.to_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_0.csv")
-for i in np.arange(0, 1.1, 0.1):
-    if i == 1.0:
+for i in np.arange(0, 1, 0.1):
+    if i == 0.9:
         results = generate_results[generate_results['0.0'].between(i, i+0.1, inclusive="both")]
     else:
         results = generate_results[generate_results['0.0'].between(i, i+0.1, inclusive="left")]
     results_list.append(results)
     #results.to_csv(f"/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_{i}.csv")
+[print(len(x)) for x in results_list]
