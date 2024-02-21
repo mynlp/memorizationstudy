d99cc01f0d641e23aea053c2a3aafc09df5ba726
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-02-21 16:39:09 +0900

    fix

--- run_generate.sh
+++ run_generate.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 #PJM -L rscgrp=share
 #PJM -L gpu=1
-#PJM -L elapse=24:00:00
+#PJM -L elapse=48:00:00
 #PJM -g gk77
 #PJM -j
 #PJM -N test
@@ -17,5 +17,6 @@ if [ -z "$RUN_ON_REMOTE" ]; then
     source /work/gk77/k77025/.zshrc
 fi
 
-## available models:
-python3 batch_generate.py --model 13m-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 32
+## available models: 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b deduped
+## available models: 14m, 31m, 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b undeduped
+python3 batch_generate.py --model 160m-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 32
