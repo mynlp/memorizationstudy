26b3712c9ade57a8b8cbf39209bbeccd4976a354
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-02-21 17:18:55 +0900

    fix

--- csv_process.py
+++ csv_process.py
@@ -3,13 +3,16 @@ import numpy as np
 
 generate_results = read_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_32_64_143000.csv")
 results_list = []
-results = generate_results[generate_results['0.0'] == 1.0]
-results_list.append(results)
+#results = generate_results[generate_results['0.0'] == 1.0]
+#results_list.append(results)
 #results.to_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_1.csv")
-results = generate_results[generate_results['0.0'] == 0]
-results_list.append(results)
+#results = generate_results[generate_results['0.0'] == 0]
+#results_list.append(results)
 #results.to_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_0.csv")
-for i in np.arange(0, 1, 0.1):
-    results = generate_results[generate_results['0.0'].between(i, i+0.1, inclusive=False)]
+for i in np.arange(0, 1.1, 0.1):
+    if i == 1.0:
+        results = generate_results[generate_results['0.0'].between(i, i+0.1, inclusive="both")]
+    else:
+        results = generate_results[generate_results['0.0'].between(i, i+0.1, inclusive="left")]
     results_list.append(results)
     #results.to_csv(f"/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_{i}.csv")
