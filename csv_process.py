005b1476dbb98fb986c7ece56b7821c379666631
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-02-21 17:10:27 +0900

    fix

--- csv_process.py
+++ csv_process.py
@@ -1,14 +1,14 @@
 from utils import *
 import numpy as np
 
-generate_results = read_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000.csv")
+generate_results = read_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_32_64_143000.csv")
 results_list = []
 results = generate_results[generate_results['0.0'] == 1.0]
 results_list.append(results)
-results.to_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_1.csv")
+#results.to_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_1.csv")
 results = generate_results[generate_results['0.0'] == 0]
 results_list.append(results)
-results.to_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_0.csv")
+#results.to_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_0.csv")
 for i in np.arange(0, 1, 0.1):
     results = generate_results[generate_results['0.0'].between(i, i+0.1, inclusive=False)]
     results_list.append(results)
