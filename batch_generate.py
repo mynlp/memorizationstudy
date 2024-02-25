ceab5785956d3613cc3e0c911ce974f7656c5dee
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-02-25 21:42:31 +0900

    fix

--- batch_generate.py
+++ batch_generate.py
@@ -108,7 +108,7 @@ def main():
     num_sequences_per_proc = total_num_sequences // NUM_PROCS
     if f"memorization_evals_{args.model}_{args.context_size}_{args.context_size+args.continuation_size}_{args.checkpoint}.csv" in os.listdir("generate_results"):
         df = pd.read_csv(f"generate_results/memorization_evals_{args.model}_{args.context_size}_{args.context_size+args.continuation_size}_{args.checkpoint}.csv", index_col=0)
-        start_idx = len(df)+1
+        start_idx = len(df)
     else:
         start_idx = num_sequences_per_proc * RANK
 
