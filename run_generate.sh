fe07fb4e98dd30e96b1540b62d3ed72fb73dc9e6
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-02-25 21:53:53 +0900

    fix

--- run_generate.sh
+++ run_generate.sh
@@ -4,9 +4,9 @@
 #PJM -L elapse=48:00:00
 #PJM -g gk77
 #PJM -j
-#PJM -N de1b3216
-#PJM -o de1b3216
-#PJM -e de1b3216
+#PJM -N de1603264
+#PJM -o de1603264
+#PJM -e de1603264
 
 #env
 #export RANK=1
@@ -19,7 +19,7 @@ fi
 
 ## available models: 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b deduped
 ## available models: 14m, 31m, 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b undeduped
-python3 batch_generate.py --model 1b-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 16
+python3 batch_generate.py --model 1b-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 64
 
 
 #python3 batch_generate.py --model 1b-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 16
