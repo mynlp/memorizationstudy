f6a603c90daeac6de2f391ca3ad21858a90ead3b
PososAgapo <bchen@kth.se>
2024-02-28 21:53:58 +0900

    correct file name

--- csv_process.py
+++ csv_process.py
@@ -15,12 +15,12 @@ for file in ["memorization_evals_70m-deduped-v0_32_48_143000.csv", "memorization
     #results.to_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_0.csv")
     for i in np.arange(0, 1, 0.1):
         if i == 0.9:
-            results = generate_results[generate_results['0.0'].between(i, i+0.1, inclusive="both")]
+            results = generate_results[generate_results['score'].between(i, i+0.1, inclusive="both")]
         else:
-            results = generate_results[generate_results['0.0'].between(i, i+0.1, inclusive="left")]
+            results = generate_results[generate_results['score'].between(i, i+0.1, inclusive="left")]
         results_list.append(results)
         #results.to_csv(f"/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_{i}.csv")
-    results = generate_results[generate_results['0.0'] == 1.0]
+    results = generate_results[generate_results["score"] == 1.0]
     results_list.append(results)
     [print(len(x)) for x in results_list]
 
