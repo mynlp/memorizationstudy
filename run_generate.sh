39e67490e2f71c7803ea6ffd7ac4ec0d0dfd00f2
PososAgapo <bchen@kth.se>
2024-02-29 18:30:49 +0900

    correct file name

--- run_generate.sh
+++ run_generate.sh
@@ -4,9 +4,9 @@
 #PJM -L elapse=48:00:00
 #PJM -g gk77
 #PJM -j
-#PJM -N de1603264
-#PJM -o de1603264
-#PJM -e de1603264
+#PJM -N de1603296
+#PJM -o de1603296
+#PJM -e de1603296
 
 #env
 #export RANK=1
@@ -20,7 +20,7 @@ fi
 ## available models: 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b deduped
 ## available models: 14m, 31m, 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b undeduped
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-python3 batch_generate.py --model 160m-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 64
+python3 batch_generate.py --model 160m-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 96
 
 
 #python3 batch_generate.py --model 1b-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 16
