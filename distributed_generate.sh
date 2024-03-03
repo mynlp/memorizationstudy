902b799d5be2321785c33880c4736c7a2e1f8d62
PososAgapo <bchen@kth.se>
2024-03-03 23:22:18 +0900

    correct file name

--- distributed_generate.sh
+++ distributed_generate.sh
@@ -4,9 +4,9 @@
 #PJM -L elapse=48:00:00
 #PJM -g gk77
 #PJM -j
-#PJM -N de706416
-#PJM -o de706416
-#PJM -e de706416
+#PJM -N de709616
+#PJM -o de709616
+#PJM -e de709616
 
 #env
 #export RANK=1
@@ -20,6 +20,6 @@ fi
 ## available models: 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b deduped
 ## available models: 14m, 31m, 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b undeduped
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-torchrun --nproc_per_node=8 distributed_generate.py --batch_size=1024 --context_size=64 --continuation_size=16 --model=70m-deduped-v0 --checkpoint=143000
+torchrun --nproc_per_node=8 distributed_generate.py --batch_size=1024 --context_size=96 --continuation_size=16 --model=70m-deduped-v0 --checkpoint=143000
 
 #python3 batch_generate.py --model 1b-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 16
