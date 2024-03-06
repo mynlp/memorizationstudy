24e51a70792fd07b2fe425f29706429248acae5b
PososAgapo <bchen@kth.se>
2024-03-06 15:32:30 +0900

    correct file name

--- csv_process.py
+++ csv_process.py
@@ -3,28 +3,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-for file in ["memorization_evals_70m-deduped-v0_32_48_143000.csv", "memorization_evals_70m-deduped-v0_32_64_143000.csv",
-            "memorization_evals_70m-deduped-v0_32_80_143000.csv", "memorization_evals_70m-deduped-v0_32_96_143000.csv",
-            "memorization_evals_70m-deduped-v0_32_128_143000.csv",
-            "memorization_evals_160m-deduped-v0_32_48_143000.csv","memorization_evals_160m-deduped-v0_32_64_143000.csv",
-            "memorization_evals_410m-deduped-v0_32_48_143000.csv",
+for file in ["memorization_evals_70m-deduped-v0_48_64_143000.csv",
+            "memorization_evals_70m-deduped-v0_64_80_143000.csv",
+            "memorization_evals_70m-deduped-v0_96_112_143000.csv"
                 ]:
     print(file)
     generate_results = pd.read_csv("generate_results/"+file, index_col=0)
-    plt.hist(generate_results['score'], bins=10, color='skyblue', edgecolor='black')
-    # 添加标题和标签
-    plt.title('Distribution of Scores')
-    plt.xlabel('Scores')
-    plt.ylabel('Frequency')
-    plt.savefig(f'{file}.png')
-    plt.show()
-    # 展示图形
-    plt.show()
-    # 创建柱状图
     results_list = []
     results = generate_results[generate_results['score'] == 0]
     results_list.append(results)
-    #results.to_csv("/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_0.csv")
     for i in np.arange(0, 1, 0.1):
         if i == 0.9:
             results = generate_results[generate_results['score'].between(i, i+0.1, inclusive="left")]
@@ -33,7 +20,6 @@ for file in ["memorization_evals_70m-deduped-v0_32_48_143000.csv", "memorization
         else:
             results = generate_results[generate_results['score'].between(i, i+0.1, inclusive="left")]
         results_list.append(results)
-        #results.to_csv(f"/work/gk77/k77025/memorizationstudy/generate_results/memorization_evals_70m-deduped-v0_143000_{i}.csv")
     results = generate_results[generate_results["score"] == 1.0]
     results_list.append(results)
     [print(len(x)) for x in results_list]
