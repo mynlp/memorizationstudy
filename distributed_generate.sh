e2bc69e412bca31b5c1440989dd2e0f7f2c4d13b
PososAgapo <bchen@kth.se>
2024-03-06 15:20:47 +0900

    correct file name

--- distributed_generate.sh
+++ distributed_generate.sh
@@ -4,9 +4,9 @@
 #PJM -L elapse=48:00:00
 #PJM -g gk77
 #PJM -j
-#PJM -N de1603296
-#PJM -o de1603296
-#PJM -e de1603296
+#PJM -N de4103232
+#PJM -o de4103232
+#PJM -e de4103232
 
 #env
 #export RANK=1pjs
@@ -18,8 +18,8 @@ if [ -z "$RUN_ON_REMOTE" ]; then
 fi
 RANK=8
 CONTEXT_SIZE=32
-CONTINUATION_SIZE=96
-MODEL=160m-deduped-v0
+CONTINUATION_SIZE=32
+MODEL=410m-deduped-v0
 
 ## available models: 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b deduped
 ## available models: 14m, 31m, 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b undeduped
