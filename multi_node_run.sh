4cc462e90333d8aba77b0d74e17678dedf2b4168
PososAgapo <bchen@kth.se>
2024-03-08 18:32:42 +0900

    correct file name

--- multi_node_run.sh
+++ multi_node_run.sh
@@ -8,14 +8,13 @@
 #PJM -N de1b3216
 #PJM -o de1b3216
 #PJM -e de1b3216
-
-$SHELL --version
-echo $0
-module load gcc/8.3.1
-module load ompi/4.1.1
-
 if [ -z "$RUN_ON_REMOTE" ]; then
     source /work/gk77/k77025/.zshrc
 fi
+echo $0
+module load gcc/8.3.1
+module load ompi/4.1.modu1
+
+
 
 mpirun -machinefile $PJM_O_NODEINF -np $PJM_MPI_PROC -map-by node ./paralle.sh
