24d1ddc8a7e6cb220dd3e5159c0b22c4d5d60bb0
PososAgapo <bchen@kth.se>
2024-03-08 16:54:40 +0900

    correct file name

--- paralle.sh
+++ paralle.sh
@@ -4,6 +4,6 @@ if [ -z "$RUN_ON_REMOTE" ]; then
     source /work/gk77/k77025/.zshrc
 fi
 
-python3 -m torch.distributed.launch --nproc_per_node=2 --nnodes=2 --node_rank=$OMPI_COMM_WORLD_RANK --master_addr=`hostname -i` --master_port=29501 distributed_generate.py
+python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=$OMPI_COMM_WORLD_RANK --master_addr=`hostname -i` --master_port=29501 distributed_generate.py
 
 #torchrun --nproc_per_node=$OMPI_COMM_WORLD_RANK distributed_generate.py --batch_size=1024 --context_size=$CONTEXT_SIZE --continuation_size=$CONTINUATION_SIZE --model=$MODEL --checkpoint=143000
