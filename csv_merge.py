769f651c6e0e0bbd09ad38dcfbd26eaf418a988f
PososAgapo <bchen@kth.se>
2024-03-10 16:57:35 +0900

    correct file name

--- csv_merge.py
+++ csv_merge.py
@@ -28,3 +28,4 @@ result.to_csv(f"generate_results/memorization_evals_{args.model_name}_{args.cont
     #os.system(f"rm generate_results/memorization_evals_{args.model_name}_{args.context_size}_{args.context_size + args.continuation_size}_{args.checkpoint}_*.csv")
 
 
+#generate_results/memorization_evals_1b-deduped-v0_32_48_143000.csv
--- paralle.sh
+++ paralle.sh
@@ -5,9 +5,13 @@ if [ -z "$RUN_ON_REMOTE" ]; then
 fi
 first_node=$(cat "$PJM_O_NODEINF" | awk 'NR==1{print $1}')
 export MASTER_ADDR=$first_node
+RANK=8
+CONTEXT_SIZE=32
+CONTINUATION_SIZE=96
+MODEL=410m-deduped-v0
 echo $MASTER_ADDR
 #python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=$OMPI_COMM_WORLD_RANK --master_addr=`hostname -i` --master_port=29501 distributed_generate.py
 cat $OMPI_COMM_WORLD_RANK >> /work/gk77/share/log
 cat $MASTER_ADDR >> /work/gk77/share/log
 #torchrun --nproc_per_node=$OMPI_COMM_WORLD_RANK distributed_generate.py --batch_size=1024 --context_size=$CONTEXT_SIZE --continuation_size=$CONTINUATION_SIZE --model=$MODEL --checkpoint=143000
-torchrun --nproc_per_node=8 --nnodes=8 --node_rank=$OMPI_COMM_WORLD_RANK --master_addr=$MASTER_ADDR --master_port=29504 distributed_generate.py #--batch_size=1024 --context_size=$CONTEXT_SIZE --continuation_size=$CONTINUATION_SIZE --model=$MODEL --checkpoint=143000
+torchrun --nproc_per_node=$RANK --nnodes=8 --node_rank=$OMPI_COMM_WORLD_RANK --master_addr=$MASTER_ADDR --master_port=29504 distributed_generate.py --batch_size=1024 --context_size=$CONTEXT_SIZE --continuation_size=$CONTINUATION_SIZE --model=$MODEL --checkpoint=143000
