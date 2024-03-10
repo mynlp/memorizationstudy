a323085bfea262684de22867308ccb2226771f28
PososAgapo <bchen@kth.se>
2024-03-11 00:18:52 +0900

    correct file name

--- multi_node_run.sh
+++ multi_node_run.sh
@@ -5,9 +5,9 @@
 #PJM --mpi proc=8
 #PJM -g gk77
 #PJM -j
-#PJM -N de1b3232
-#PJM -o de1b3232
-#PJM -e de1b3232
+#PJM -N de1b3264
+#PJM -o de1b3264
+#PJM -e de1b3264
 #if [ -z "$RUN_ON_REMOTE" ]; then
 #    source /work/gk77/k77025/.zshrc
 #fi
