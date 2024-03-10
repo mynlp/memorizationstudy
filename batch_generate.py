28cdfece71586ca1623932e4c8f0a4d9f9127378
PososAgapo <bchen@kth.se>
2024-03-10 17:57:35 +0900

    correct file name

--- batch_generate.py
+++ batch_generate.py
@@ -124,6 +124,7 @@ def main():
     model = GPTNeoXForCausalLM.from_pretrained(
         f"EleutherAI/pythia-{args.model}",
         revision=f'step{args.checkpoint}',
+        load_in_8_bits=True
     )
     model = model.half()
     if torch.cuda.device_count() > 1:
