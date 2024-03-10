7da02b8bbdca12c4b47b128d2231541cc2eab4c7
PososAgapo <bchen@kth.se>
2024-03-11 00:16:17 +0900

    correct file name

--- paralle.sh
+++ paralle.sh
@@ -7,8 +7,8 @@ first_node=$(cat "$PJM_O_NODEINF" | awk 'NR==1{print $1}')
 export MASTER_ADDR=$first_node
 RANK=8
 CONTEXT_SIZE=32
-CONTINUATION_SIZE=32
-MODEL=1b-deduped-v0
+CONTINUATION_SIZE=96
+MODEL=410m-deduped-v0
 echo $MASTER_ADDR
 #python3 -m torch.distributed.launch --nproc_per_node=1 --nnodes=1 --node_rank=$OMPI_COMM_WORLD_RANK --master_addr=`hostname -i` --master_port=29501 distributed_generate.py
 cat $OMPI_COMM_WORLD_RANK >> /work/gk77/share/log
