192e11d3648b403329fce5e8d38628724b8cc6b5
PososAgapo <bchen@kth.se>
2024-03-12 14:09:52 +0900

    correct file name

--- multi_node_run.sh
+++ multi_node_run.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 #PJM -L rscgrp=regular-a
-#PJM -L node=8
-#PJM -L elapse=24:00:00
+#PJM -L node=4
+#PJM -L elapse=48:00:00
 #PJM --mpi proc=8
 #PJM -g gk77
 #PJM -j
-#PJM -N de4103296
-#PJM -o de4103296
-#PJM -e de4103296
+#PJM -N de4103264
+#PJM -o de4103264
+#PJM -e de4103264
 #if [ -z "$RUN_ON_REMOTE" ]; then
 #    source /work/gk77/k77025/.zshrc
 #fi
@@ -26,4 +26,4 @@ cat $PJM_O_NODEINF >> /work/gk77/share/log
 #CONTINUATION_SIZE=$4
 #BATCH_SIZE=$5
 #MODEL=$6
-mpirun -machinefile $PJM_O_NODEINF -np $PJM_MPI_PROC -map-by node ./paralle.sh 8 8 32 96 1024 410m-deduped-v0
+mpirun -machinefile $PJM_O_NODEINF -np $PJM_MPI_PROC -map-by node ./paralle.sh 8 8 32 64 1024 410m-deduped-v0
