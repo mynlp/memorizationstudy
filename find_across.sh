7b45bd02d7edc835a3c8d930af05910e071b336a
PososAgapo <bchen@kth.se>
2024-03-25 18:41:49 +0900

    correct file name

---
+++ find_across.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+#PJM -L rscgrp=share
+#PJM -L gpu=1
+#PJM -L elapse=32:00:00
+#PJM -g gk77
+#PJM -j
+#PJM -N findacross
+#PJM -o findacross
+#PJM -e findacross
+
+#env
+#export RANK=1pjs
+#export MODEL=70m-deduped-v0
+#export CHECKPOINT=143000
+#
+python find_across.py
+#python3 batch_generate.py --model 1b-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 16
