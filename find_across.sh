78e55e908dcf545fb997ac028e8be55ab4c51d0c
PososAgapo <bchen@kth.se>
2024-03-26 16:24:43 +0900

    correct file name

--- find_across.sh
+++ find_across.sh
@@ -13,5 +13,8 @@
 #export MODEL=70m-deduped-v0
 #export CHECKPOINT=143000
 #
+if [ -z "$RUN_ON_REMOTE" ]; then
+    source /work/gk77/k77025/.zshrc
+fi
 python find_cross.py
 #python3 batch_generate.py --model 1b-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 16
