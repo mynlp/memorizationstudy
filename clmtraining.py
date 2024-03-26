9a96dcaa21c2f5ecdfc5c8bf87b634c7ff31841e
PososAgapo <bchen@kth.se>
2024-03-26 13:39:11 +0900

    correct file name

--- clmtraining.py
+++ clmtraining.py
@@ -6,13 +6,14 @@ import math
 import torch
 
 
+
 def batchfy(data):
   input_batch = []
   for input_ids in (data):
       input_batch.append(input_ids)
   return {"input_ids": input_batch}
 
-ds = Dataset.from_dict({"input_ids": torch.load("cross_remembered/context_tokens.pt").view(-1,2049)})
+raw_dataset = Dataset.from_dict({"input_ids": torch.load("cross_remembered/context_tokens.pt").view(-1,2049)})
 model_name = "EleutherAI/pythia-160m-deduped-v0"
 CHECKPOINT = 143000
 context_length = 128
@@ -21,6 +22,9 @@ tokenizer = AutoTokenizer.from_pretrained(
   revision=f"step{CHECKPOINT}",
   cache_dir=f"./pythia-160m-deduped/step{CHECKPOINT}",
 )
+tokenizer.pad_token = tokenizer.eos_token
+data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+
 config = AutoConfig.from_pretrained(
     "gpt2",
     vocab_size=len(tokenizer),
@@ -30,30 +34,41 @@ config = AutoConfig.from_pretrained(
 )
 
 
-eli5 = eli5.flatten()
-tokenized_eli5 = eli5.map(
-    preprocess_function,
+raw_dataset = raw_dataset.flatten()
+tokenized_eli5 = raw_dataset.map(
+    batchfy,
     batched=True,
-    remove_columns=eli5["train"].column_names,
 )
 block_size = 128
 lm_dataset = tokenized_eli5.map(group_texts, batched=True)
 tokenizer.pad_token = tokenizer.eos_token
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
 model = AutoModelForCausalLM.from_pretrained("distilgpt2")
-training_args = TrainingArguments(
-    output_dir="small_demo",
-    evaluation_strategy="epoch",
-    learning_rate=2e-5,
-    weight_decay=0.01,
+args = TrainingArguments(
+    output_dir="codeparrot-ds",
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    evaluation_strategy="steps",
+    eval_steps=5_000,
+    logging_steps=5_000,
+    gradient_accumulation_steps=8,
+    num_train_epochs=1,
+    weight_decay=0.1,
+    warmup_steps=1_000,
+    lr_scheduler_type="cosine",
+    learning_rate=5e-4,
+    save_steps=5_000,
+    fp16=True,
+    push_to_hub=True,
 )
 
 trainer = Trainer(
     model=model,
-    args=training_args,
-    train_dataset=lm_dataset["train"],
-    eval_dataset=lm_dataset["test"],
+    tokenizer=tokenizer,
+    args=args,
     data_collator=data_collator,
+    train_dataset=lm_dataset["train"],
+    eval_dataset=lm_dataset["valid"],
 )
 
 trainer.train()
--- find_across.sh
+++ find_across.sh
@@ -13,5 +13,5 @@
 #export MODEL=70m-deduped-v0
 #export CHECKPOINT=143000
 #
-python find_across.py
+python find_cross.py
 #python3 batch_generate.py --model 1b-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 16
