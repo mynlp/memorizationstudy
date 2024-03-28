378aeee8c6214e6296292dae361e0420c91497f1
PososAgapo <bchen@kth.se>
2024-03-28 16:31:07 +0900

    correct file name

---
+++ distribution_analysis.py
@@ -0,0 +1,78 @@
+import pandas as pd
+from transformers import GPTNeoXForCausalLM, AutoTokenizer,  AutoModelForCausalLM,  LogitsProcessorList, MinLengthLogitsProcessor, StoppingCriteriaList,  MaxLengthCriteria
+from pythia.utils.mmap_dataset import MMapIndexedDataset
+from transformers import GPTNeoXForCausalLM, AutoTokenizer
+import torch
+from utils import *
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import random
+
+random.seed(42)
+model_name = "EleutherAI/pythia-70m-deduped-v0"
+CHECKPOINT= 143000
+model = GPTNeoXForCausalLM.from_pretrained(
+  model_name,
+  revision=f"step{CHECKPOINT}",
+  cache_dir=f"./pythia-70m-deduped/step{CHECKPOINT}",
+)
+model.eval().cuda()
+tokenizer = AutoTokenizer.from_pretrained(
+  model_name,
+  revision=f"step{CHECKPOINT}",
+  cache_dir=f"./pythia-70m-deduped/step{CHECKPOINT}",
+)
+model.generation_config.pad_token_id = model.generation_config.eos_token_id
+model.generation_config.output_hidden_states = True
+model.generation_config.output_attentions = True
+model.generation_config.output_scores = True
+model.generation_config.return_dict_in_generate = True
+
+
+prefix = 'undeduped_merge/document.bin'
+if "deduped" in model_name:
+    prefix = 'deduped_merge/document.bin'
+print(prefix)
+buff_size = 2049*1024*2
+print("Building dataset")
+mmap_ds = MMapIndexedDataset(prefix, skip_warmup=True)
+
+df = pd.read_csv("generate_results/memorization_evals_70m-deduped-v0_32_48_143000.csv", index_col=0)
+df_full_memorization = df[df['score'] == 1]
+df_not_full_memorization = df[df['score'] == 0]
+df_half_memorization = df[df['score'] == 0.5]
+
+
+idx_full_memorization = df_full_memorization["idx"].tolist()
+idx_not_full_memorization = df_not_full_memorization["idx"].tolist()
+idx_half_memorization = df_half_memorization["idx"].tolist()
+
+num_points = 30
+highest_probability_memorized = logits_obtain(random.sample(idx_full_memorization,num_points), model, 32, 16)
+highest_probability_unmemorized = logits_obtain(random.sample(idx_not_full_memorization,num_points), model, 32, 16)
+
+plt.figure(figsize=(12, 8))  # 创建图像
+
+# 使用不同的颜色和样式绘制两类数据
+for i in range(30):
+    plt.plot(range(16), [x[i] for x in highest_probability_memorized], color='blue', linestyle='-', alpha=0.5)  # 类别1的样式
+    plt.plot(range(16), [x[i] for x in highest_probability_unmemorized], color='red', linestyle='--', alpha=0.5)  # 类别2的样式
+
+# 创建图例来说明每个颜色和样式代表的类别
+plt.plot([], [], color='blue', linestyle='-', label='Category 1')  # 类别1的图例
+plt.plot([], [], color='red', linestyle='--', label='Category 2')  # 类别2的图例
+
+plt.legend()  # 显示图例
+plt.show()  # 显示图像
+
+# plt.title('t-SNE Visualization')
+# plt.legend()
+# plt.savefig(f'tsne_visualization_{num_points}_{stragety}_{token}.png')
+# plt.show()
+
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -45,5 +45,25 @@ def embedding_obtain(dataset, model, idx_list, context_size, continuation_size):
         generations.hidden_states = results
         return [generations, accuracies]
 
+def logits_obtain(dataset, model, idx_list, context_size, continuation_size):
+    batched_context_tokens = []
+    batched_true_continuation = []
+    for idx in idx_list:
+        data = dataset[idx]
+        context_tokens = data[:context_size].tolist()
+        true_continuation = data[context_size:context_size + continuation_size].tolist()
+        batched_context_tokens.append(context_tokens)
+        batched_true_continuation.append(true_continuation)
+
+    model_outputs = model.generate(context_tokens, temperature=0.0, top_k=0, top_p=0, max_length=context_size+continuation_size, min_length=context_size+continuation_size)
+    logits = model_outputs["scores"]
+    highest_probability_at_idx = []
+    for idx in range(continuation_size):
+        probability_scores = torch.nn.functional.softmax(logits[idx], dim=1)
+        highest_probability_at_idx.append(probability_scores.max(1)[0])
+    return highest_probability_at_idx
+
+
+
 
 
