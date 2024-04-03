293d4a1f4444ea5531a40713c069d92640f6755e
PososAgapo <bchen@kth.se>
2024-04-03 17:41:47 +0900

    correct file name

--- run_generate.sh
+++ run_generate.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
-#PJM -L rscgrp=regular-a
-#PJM -L node=1
-#PJM -L elapse=48:00:00
+#PJM -L rscgrp=share-debug
+#PJM -L gpu=1
+#PJM -L elapse=00:30:00
 #PJM -g gk77
 #PJM -j
-#PJM -N de1603264
-#PJM -o de1603264
-#PJM -e de1603264
+#PJM -N shortjob
+#PJM -o shortjob
+#PJM -e shortjob
 
 #env
 #export RANK=1
@@ -19,8 +19,5 @@ fi
 
 ## available models: 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b deduped
 ## available models: 14m, 31m, 70m, 160m, 410m, 1b, 1.4b, 2.8b, 6.9b, 12b undeduped
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-python3 batch_generate.py --model 160m-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 32
-
-
+python embedding_drift.py
 #python3 batch_generate.py --model 1b-deduped-v0 --checkpoint 143000 --batch_size 1024 --context_size 32 --continuation_size 16
---
+++ short_job.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+#PJM -L rscgrp=regular-o
+#PJM -L node=25
+#PJM -L elapse=48:00:00
+#PJM -g gk77
+#PJM -j
+#PJM -N undeduped_merge_log
+#PJM -o undeduped_merge_log
+#PJM -e undeduped_merge_log
+
+source /work/gk77/k77025/.zshrc
+cd pythia
+python3 utils/unshard_memmap.py --input_file "/work/gk77/k77025/memorizationstudy/undeduped_data/datasets--EleutherAI--pile-deduped-pythia-preshuffled/snapshots/4647773ea142ab1ff5694602fa104bbf49088408/document-00000-of-00020.bin" --num_shards 21 --output_dir "/work/gk77/k77025/memorizationstudy/undeduped_merge"
