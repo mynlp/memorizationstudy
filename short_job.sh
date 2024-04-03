7d7c03d1528a67b320b1695a78eb12cb1188d5b7
PososAgapo <bchen@kth.se>
2024-04-03 18:55:23 +0900

    correct file name

--- short_job.sh
+++ short_job.sh
@@ -1,13 +1,15 @@
 #!/bin/zsh
-#PJM -L rscgrp=debug-a
-#PJM -L node=25
-#PJM -L elapse=48:00:00
+#PJM -L rscgrp=share-debug
+#PJM -L gpt=1
+#PJM -L elapse=00:30:00
 #PJM -g gk77
 #PJM -j
-#PJM -N undeduped_merge_log
-#PJM -o undeduped_merge_log
-#PJM -e undeduped_merge_log
+#PJM -N shortjob
+#PJM -o shortjob
+#PJM -e shortjob
 
-source /work/gk77/k77025/.zshrc
-cd pythia
-python3 utils/unshard_memmap.py --input_file "/work/gk77/k77025/memorizationstudy/undeduped_data/datasets--EleutherAI--pile-deduped-pythia-preshuffled/snapshots/4647773ea142ab1ff5694602fa104bbf49088408/document-00000-of-00020.bin" --num_shards 21 --output_dir "/work/gk77/k77025/memorizationstudy/undeduped_merge"
+if [ -z "$RUN_ON_REMOTE" ]; then
+    source /work/gk77/k77025/.zshrc
+fi
+
+python embedding_drift.py
