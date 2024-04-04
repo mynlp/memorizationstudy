f7902f5a8eaa793e7e440bce43bcf62fcb2d0b8a
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-04-04 18:17:54 +0900

    fix

--- distribution_analysis.py
+++ distribution_analysis.py
@@ -52,41 +52,53 @@ highest_probability_memorized = logits_obtain(mmap_ds, model,  random.sample(idx
 highest_probability_unmemorized = logits_obtain(mmap_ds, model,  random.sample(idx_full_memorization,num_points), 32, 16)
 
 plt.figure(figsize=(12, 8))  # 创建图像
-
-# 使用不同的颜色和样式绘制两类数据
+# by-line plot
 memorized_values = [np.array([x[i].cpu() for x in highest_probability_memorized])
                     for i in range(num_points)]
 unmemorized_values = [np.array([x[i].cpu() for x in highest_probability_unmemorized])
                       for i in range(num_points)]
+for values in memorized_values:
+    plt.plot(range(16), values, color='blue', linestyle='-', alpha=0.1)  # 使用较低的透明度来避免图形过于拥挤
 
-# 计算平均值和方差
-memorized_mean = np.mean(memorized_values, axis=0)
-memorized_std = np.std(memorized_values, axis=0)
-unmemorized_mean = np.mean(unmemorized_values, axis=0)
-unmemorized_std = np.std(unmemorized_values, axis=0)
-
-# 绘制平均值
-plt.plot(range(16), memorized_mean, color='blue', linestyle='-')
-plt.plot(range(16), unmemorized_mean, color='red', linestyle='-')
-
-# 添加阴影显示方差
-plt.fill_between(range(16), memorized_mean - memorized_std, memorized_mean + memorized_std,
-                 color='blue', alpha=0.2)
-plt.fill_between(range(16), unmemorized_mean - unmemorized_std, unmemorized_mean + unmemorized_std,
-                 color='red', alpha=0.2)
+# 绘制未记忆化的每一条线
+for values in unmemorized_values:
+    plt.plot(range(16), values, color='red', linestyle='-', alpha=0.1)  # 使用较低的透明度
 
 # 创建图例来说明每个颜色和样式代表的类别
-plt.plot([], [], color='blue', linestyle='-', label='Category 1')  # 类别1的图例
-plt.plot([], [], color='red', linestyle='-', label='Category 2')  # 类别2的图例
+plt.plot([], [], color='blue', linestyle='-', label='Memorized')  # 添加一个看不见的线作图例表示记忆化
+plt.plot([], [], color='red', linestyle='-', label='Unmemorized')  # 添加一个看不见的线作图例表示未记忆化
 
 plt.legend()  # 显示图例
-plt.savefig(f'distribution.png')
-plt.show()  # 显示图像
+plt.savefig(f'distribution_individual_lines.png')  # 保存图形
+plt.show()  # 显示图形
 
-# plt.title('t-SNE Visualization')
-# plt.legend()
-# plt.savefig(f'tsne_visualization_{num_points}_{stragety}_{token}.png')
-# plt.show()
+# code for mean and std plot
+# # 使用不同的颜色和样式绘制两类数据
+
+#
+# # 计算平均值和方差
+# memorized_mean = np.mean(memorized_values, axis=0)
+# memorized_std = np.std(memorized_values, axis=0)
+# unmemorized_mean = np.mean(unmemorized_values, axis=0)
+# unmemorized_std = np.std(unmemorized_values, axis=0)
+#
+# # 绘制平均值
+# plt.plot(range(16), memorized_mean, color='blue', linestyle='-')
+# plt.plot(range(16), unmemorized_mean, color='red', linestyle='-')
+#
+# # 添加阴影显示方差
+# plt.fill_between(range(16), memorized_mean - memorized_std, memorized_mean + memorized_std,
+#                  color='blue', alpha=0.2)
+# plt.fill_between(range(16), unmemorized_mean - unmemorized_std, unmemorized_mean + unmemorized_std,
+#                  color='red', alpha=0.2)
+#
+# # 创建图例来说明每个颜色和样式代表的类别
+# plt.plot([], [], color='blue', linestyle='-', label='Category 1')  # 类别1的图例
+# plt.plot([], [], color='red', linestyle='-', label='Category 2')  # 类别2的图例
+#
+# plt.legend()  # 显示图例
+# plt.savefig(f'distribution.png')
+# plt.show()  # 显示图像
 
 
 
