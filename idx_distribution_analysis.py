dd5c284b92cd070374943cf3856367f023319daa
πόσο σ' αγαπώ <bwchen@g.ecc.u-tokyo.ac.jp>
2024-04-04 15:23:16 +0900

    fix

--- idx_distribution_analysis.py
+++ idx_distribution_analysis.py
@@ -1,18 +1,31 @@
 import pandas as pd
-from transformers import GPTNeoXForCausalLM, AutoTokenizer,  AutoModelForCausalLM,  LogitsProcessorList, MinLengthLogitsProcessor, StoppingCriteriaList,  MaxLengthCriteria
-from pythia.utils.mmap_dataset import MMapIndexedDataset
-from transformers import GPTNeoXForCausalLM, AutoTokenizer
-import torch
-from utils import *
-from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
-import random
 
+# 加载数据
 df = pd.read_csv("generate_results/memorization_evals_70m-deduped-v0_32_48_143000.csv", index_col=0)
+
+# 绘制整体idx分布
+plt.hist(df['idx'], bins=30, alpha=0.5, label='Total Distribution', color='grey')
+
+# 提取记忆化（score为1）的idx并绘制其分布
 df_full_memorization = df[df['score'] == 1]
-ax = df_full_memorization["idx"].plot(kind="hist", bins=30, title="Distribution of memorized sequences")
-ax.figure.savefig("memorized_sequences_distribution.png")
+plt.hist(df_full_memorization['idx'], bins=30, alpha=0.75, label='Memorized Sequences', color='blue')
+
+# 提取未记忆化（score为0）的idx并绘制其分布
+df_unmemorized = df[df['score'] == 0]
+plt.hist(df_unmemorized['idx'], bins=30, alpha=0.75, label='Unmemorized Sequences', color='red')
+
+# 添加图例、标题和坐标轴标签
+plt.legend()
+plt.title('Distribution of idx in Total, Memorized, and Unmemorized Sequences')
+plt.xlabel('idx')
+plt.ylabel('Count')
+
+# 保存图像文件
+plt.savefig("distribution_comparison.png")
+
+# 显示图形
+plt.show()
+
+# 清除当前图形
 plt.clf()
-df_full_memorization = df[df['score'] == 0]
-ax = df_full_memorization["idx"].plot(kind="hist", bins=30, title="Distribution of unmemorized sequences")
-ax.figure.savefig("unmemorized_sequences_distribution.png")
