e8facfbb8b20b455ec51c0274d8bbcde98d250df
PososAgapo <bchen@kth.se>
2024-04-16 12:02:53 +0900

    correct file name

--- utils.py
+++ utils.py
@@ -16,6 +16,7 @@ class InstructDataset(Dataset):
         for j in tqdm(json_list):
             # open_qaなど文脈情報が必要ない場合はinputカラムがないため、
             # inputカラムありなしでテンプレート文を分けている。
+            pdb.set_trace()
             if 'context' in j:
                 source_text = PROMPT_DICT['prompt_input'].format_map(j)
             else:
