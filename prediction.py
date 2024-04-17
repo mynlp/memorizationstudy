dfc4d2deb921fd7a2a7d64a23e8c978fc2127ce0
PososAgapo <bchen@kth.se>
2024-04-17 14:39:38 +0900

    correct file name

--- prediction.py
+++ prediction.py
@@ -30,10 +30,10 @@ class BayesianLSTM(PyroModule):
     def __init__(self, input_size=1, hidden_size=50, out_size=1):
         super().__init__()
         self.lstm = PyroModule[nn.LSTM](input_size, hidden_size, batch_first=True).cuda()
-        self.lstm.weight_ih_l0 = PyroSample(dist.Normal(0., 1.).expand([4 * hidden_size, input_size]).to_event(2)).cuda()
-        self.lstm.weight_hh_l0 = PyroSample(dist.Normal(0., 1.).expand([4 * hidden_size, hidden_size]).to_event(2)).cuda()
-        self.linear = PyroModule[nn.Linear](hidden_size, out_size).cuda()
-        self.linear.weight = PyroSample(dist.Normal(0., 1.).expand([out_size, hidden_size]).to_event(2)).cuda()
+        self.lstm.weight_ih_l0 = PyroSample(dist.Normal(0., torch.tensor(1).cuda()).expand([4 * hidden_size, input_size]).to_event(2))
+        self.lstm.weight_hh_l0 = PyroSample(dist.Normal(0., torch.tensor(1).cuda()).expand([4 * hidden_size, hidden_size]).to_event(2))
+        self.linear = PyroModule[nn.Linear](hidden_size, out_size)
+        self.linear.weight = PyroSample(dist.Normal(0., torch.tensor(1).cuda()).expand([out_size, hidden_size]).to_event(2))
 
     def forward(self, x, y=None):
         x, _ = self.lstm(x.cuda())
