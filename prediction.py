3962867f236d719ffee480b132ca3e3504e37e70
PososAgapo <bchen@kth.se>
2024-04-21 20:26:49 +0900

    correct file name

--- prediction.py
+++ prediction.py
@@ -39,6 +39,7 @@ def infer(predictor, embeddings, repeats=10):
         for _ in range(repeats):
             scores = predictor.infer(embeddings.float().cuda())
             scores_list.append(scores.squeeze())
+    pdb.set_trace()
     scores_arr = torch.stack(scores_list, dim=1)
     return scores_arr.mean(dim=0), scores_arr.var(dim=0)
 
