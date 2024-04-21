6c189f26a422ae76068d3a1f933fe46fd6e75147
PososAgapo <bchen@kth.se>
2024-04-21 19:36:35 +0900

    correct file name

---
+++ prediction.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+#PJM -L rscgrp=share
+#PJM -L gpu=1
+#PJM -L elapse=48:00:00
+#PJM -g gk77
+#PJM -j
+#PJM -N prediction
+#PJM -o prediction
+#PJM -e prediction
+
+if [ -z "$RUN_ON_REMOTE" ]; then
+    source /work/gk77/k77025/.zshrc
+fi
+
+python prediction.py
+#python batch_generate.py
