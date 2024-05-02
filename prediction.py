855f177b3d32d79f141308f0612df29dc58f41e6
PososAgapo <bchen@kth.se>
2024-05-02 12:36:46 +0900

    correct file nameg

--- prediction.py
+++ prediction.py
@@ -48,17 +48,17 @@ def infer(predictor, embeddings, entropy, repeats=50):
 
 
 args = argparse.ArgumentParser()
-args.add_argument("--model_size", type=str, default="70m")
+args.add_argument("--model_size", type=str, default="410m")
 args.add_argument("--context", type=int, default=128)
 args.add_argument("--continuation", type=int, default=16)
 args.add_argument("--checkpoint", type=int, default=143000)
 args.add_argument("--seed", type=int, default=42)
-args.add_argument("--epoch", type=int, default=40)
+args.add_argument("--epoch", type=int, default=30)
 args.add_argument("--hidden_size", type=int, default=512)
 args.add_argument("--load_cache", type=bool, default=True)
 args.add_argument("--model_type", type=str, default="lstm")
 args.add_argument("--batch_size", type=int, default=128)
-args.add_argument("--lr", type=float, default=5e-4)
+args.add_argument("--lr", type=float, default=1e-4)
 args = args.parse_args()
 embedding_size_dict = {"70m": 512, "160m": 768, "410m": 1024, "1b": 2048, "2.8b": 2560, "6.9b": 4096, "12b": 5120}
 embedding_size = embedding_size_dict[args.model_size]
