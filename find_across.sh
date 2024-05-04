1bdfac0c19ac6263082df6f94babb64114d96f97
PososAgapo <bchen@kth.se>
2024-05-04 23:21:08 +0900

    correct file nameg

--- find_across.sh
+++ find_across.sh
@@ -4,9 +4,9 @@
 #PJM -L elapse=24:00:00
 #PJM -g gk77
 #PJM -j
-#PJM -N findacross410m
-#PJM -o findacross410m
-#PJM -e findacross410m
+#PJM -N findacross410m64
+#PJM -o findacross410m64
+#PJM -e findacross410m64
 
 #env
 #export RANK=1pjs
@@ -16,4 +16,4 @@
 if [ -z "$RUN_ON_REMOTE" ]; then
     source /work/gk77/k77025/.zshrc
 fi
-python find_cross.py --model_size 410m  --batch_size 50 --context_size 32 --continuation_size 48 --num_samples 2000
+python find_cross.py --model_size 410m  --batch_size 50 --context_size 32 --continuation_size 64 --num_samples 2000
