d067239801ad8479ae78762bab5c6dc3d1810147
PososAgapo <bchen@kth.se>
2024-05-07 14:04:36 +0900

    correct file nameg

--- prediction.sh
+++ prediction.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 #PJM -L rscgrp=regular-a
-#PJM -L node=2
-#PJM -L elapse=24:00:00
+#PJM -L node=1
+#PJM -L elapse=48:00:00
 #PJM -g gk77
 #PJM -j
-#PJM -N pred1b48
-#PJM -o pred1b48
-#PJM -e pred1b48
+#PJM -N pred70m16
+#PJM -o pred70m16
+#PJM -e pred70m16
 
 if [ -z "$RUN_ON_REMOTE" ]; then
     source /work/gk77/k77025/.zshrc
 fi
 
-python prediction.py --model_size 1b --context_size 32 --continuation_size 48
+python prediction.py --model_size 70m --context_size 32 --continuation_size 48
 #python batch_generate.py
