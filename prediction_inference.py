4422b0405724221a9c6c1dbde119d646e202f3e1
PososAgapo <bchen@kth.se>
2024-05-16 01:02:39 +0900

    correct file nameg

--- prediction_inference.py
+++ prediction_inference.py
@@ -14,9 +14,13 @@ import os
 from datasets import DatasetDict
 import pdb
 
+
 def format_example(example):
-    tokens, labels, embeddings, prediction, entropy = example['token'], example['label'], example["embedding"], example["prediction"], example["entropy"]
-    return {'input_ids': tokens, 'labels': labels, 'embedding': embeddings, 'prediction': prediction, 'entropy': entropy}
+    tokens, labels, embeddings, prediction, entropy = example['token'], example['label'], example["embedding"], example[
+        "prediction"], example["entropy"]
+    return {'input_ids': tokens, 'labels': labels, 'embedding': embeddings, 'prediction': prediction,
+            'entropy': entropy}
+
 
 def output_probability(idx, tokens, probs, prediction, tokenizer, classificaiton_results, predcited, f):
     print("\n###Begining of Sentence###\n")
@@ -24,7 +28,6 @@ def output_probability(idx, tokens, probs, prediction, tokenizer, classificaiton
     sent_token = [tokenizer.decode(token_id) for token_id in tokens[idx]]
     succeded["tokens"].append(sent_token)
     succeded["probability"].append(probs)
-    probs[idx]
     for sent_idx, token in enumerate(sent_token[args.context_size:]):
         if predcited[idx][sent_idx] == 1:
             print(f"Memorized Probability of token {token} at {idx}:{probs[idx][sent_idx][1]}")
@@ -40,7 +43,9 @@ def output_probability(idx, tokens, probs, prediction, tokenizer, classificaiton
                 f.write("The actual label should be memorized\n")
     print(tokenizer.decode(tokens[idx]))
     print("\n###End of Sentence###\n")
-    f.write(tokenizer.decode(tokens[idx]))
+    f.write(tokenizer.decode(tokens[idx]) + "\n")
+
+
 def evaluate(predictor, dataloader):
     predictor.eval()  # Set the model to evaluation mode
     total_loss = 0
@@ -57,7 +62,7 @@ def evaluate(predictor, dataloader):
             classification_loss = classification_loss_fn(classes.squeeze().view(-1, 2),
                                                          prediction.type(torch.int64).view(-1).to(device))
             row_data_size += prediction.shape[0]
-            token_data_size += prediction.shape[0]*prediction.shape[1]
+            token_data_size += prediction.shape[0] * prediction.shape[1]
             #pdb.set_trace()
             classificaiton_results = classes.squeeze().argmax(dim=2) == prediction.type(torch.int64).to(device)
             row_eq_res = torch.all(classificaiton_results, dim=1)
@@ -66,7 +71,7 @@ def evaluate(predictor, dataloader):
             total_loss += loss.item()
             counter += classificaiton_results
             full＿acc_counter += row_eq_res.float().sum()
-    return total_loss / len(dataloader), counter/token_data_size, full＿acc_counter/row_data_size
+    return total_loss / len(dataloader), counter / token_data_size, full＿acc_counter / row_data_size
 
 
 def infer(predictor, embeddings, entropy, repeats=50):
@@ -99,9 +104,9 @@ num_gpus = torch.cuda.device_count()
 model_name = f"EleutherAI/pythia-{args.model_size}-deduped-v0"
 
 tokenizer = AutoTokenizer.from_pretrained(
-  model_name,
-  revision=f"step{143000}",
-  cache_dir=f"./pythia-{args.model_size}-deduped/step{143000}",
+    model_name,
+    revision=f"step{143000}",
+    cache_dir=f"./pythia-{args.model_size}-deduped/step{143000}",
 )
 if num_gpus > 1:
     print("Number of available GPUs: ", num_gpus)
@@ -132,7 +137,7 @@ token_data_size = 0
 row_data_size = 0
 counter = 0
 full＿acc_counter = 0
-succeded = {"tokens":[],"probability":[]}
+succeded = {"tokens": [], "probability": []}
 memorized_dict = {}
 f = open("visulization.txt", "w")
 with torch.no_grad():  # Do not calculate gradient since we are only evaluating
@@ -145,25 +150,28 @@ with torch.no_grad():  # Do not calculate gradient since we are only evaluating
         probs = torch.exp(classes)
         classificaiton_results = classes.squeeze().argmax(dim=2) == prediction.type(torch.int64).to(device)
         row_eq_res = torch.all(classificaiton_results, dim=1)
-        mem_score = prediction.sum(dim=1)/16
-        for idx, score in enumerate(classificaiton_results.sum(dim=1)/16):
+        mem_score = prediction.sum(dim=1) / 16
+        for idx, score in enumerate(classificaiton_results.sum(dim=1) / 16):
             if score == 1:
                 print("Prediction Score: 1")
                 print(f"Memorization Score:{mem_score[idx]}")
+                f.write(f"Prediction Score: 1\n")
+                f.write(f"Memorization Score:{mem_score[idx]}\n")
                 if mem_score[idx].item() not in memorized_dict:
                     memorized_dict[mem_score[idx].item()] = 1
                 else:
                     memorized_dict[mem_score[idx].item()] += 1
-                f.write(f"Prediction Score: 1\n")
-                output_probability(idx, tokens, probs, prediction, tokenizer, classificaiton_results, classes.squeeze().argmax(dim=2), f)
+                output_probability(idx, tokens, probs, prediction, tokenizer, classificaiton_results,
+                                   classes.squeeze().argmax(dim=2), f)
             elif score == 0.5:
                 print("Prediction Score: 0.5")
-                output_probability(idx, tokens, probs, prediction, tokenizer, classificaiton_results, classes.squeeze().argmax(dim=2), f)
+                print(f"Memorization Score:{mem_score[idx]}")
+                f.write(f"Prediction Score: 0.5\n")
+                f.write(f"Memorization Score:{mem_score[idx]}\n")
+                output_probability(idx, tokens, probs, prediction, tokenizer, classificaiton_results,
+                                   classes.squeeze().argmax(dim=2), f)
         #classificaiton_results = classificaiton_results.float().sum()
 f.close()
 print(memorized_dict)
-
-
-
 
 #{0.0625: 54, 0.25: 13, 0.375: 16, 0.9375: 27, 0.6875: 22, 0.0: 105, 0.8125: 19, 0.4375: 12, 0.625: 20, 0.75: 18, 0.1875: 25, 0.875: 25, 0.5625: 9, 0.125: 30, 0.5: 20, 0.3125: 15, 1.0: 1}
