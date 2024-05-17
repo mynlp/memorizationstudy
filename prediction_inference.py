bcf7370cc152991eb95d558699b753441565bf16
PososAgapo <bchen@kth.se>
2024-05-17 13:21:43 +0900

    correct file nameg

--- prediction_inference.py
+++ prediction_inference.py
@@ -84,7 +84,7 @@ def infer(predictor, embeddings, entropy, repeats=50):
 args = argparse.ArgumentParser()
 args.add_argument("--model_size", type=str, default="410m")
 args.add_argument("--context_size", type=int, default=32)
-args.add_argument("--continuation_size", type=int, default=16)
+args.add_argument("--continuation_size", type=int, default=32)
 args.add_argument("--checkpoint", type=int, default=143000)
 args.add_argument("--seed", type=int, default=42)
 args.add_argument("--epoch", type=int, default=30)
