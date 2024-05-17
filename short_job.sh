51d87307d05073b26002685183554f7725d2971e
PososAgapo <bchen@kth.se>
2024-05-17 14:04:47 +0900

    correct file nameg

--- short_job.sh
+++ short_job.sh
@@ -1,16 +1,16 @@
 #!/bin/zsh
 #PJM -L rscgrp=share
 #PJM -L gpu=1
-#PJM -L elapse=48:00:00
+#PJM -L elapse=12:00:00
 #PJM -g gk77
 #PJM -j
-#PJM -N shortjob
-#PJM -o shortjob
-#PJM -e shortjob
+#PJM -N distribution
+#PJM -o distribution
+#PJM -e distribution
 
 if [ -z "$RUN_ON_REMOTE" ]; then
     source /work/gk77/k77025/.zshrc
 fi
-python prediction_inference.py
-#python distribution_analysis.py
+#python prediction_inference.py
+python distribution_analysis.py
 #python batch_generate.py
