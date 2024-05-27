2998aea97dc03844f6f9f1f6bed2aa0ff2d8546f
PososAgapo <bchen@kth.se>
2024-05-27 14:23:27 +0900

    correct file nameg

--- embedding_drift.py
+++ embedding_drift.py
@@ -91,35 +91,66 @@ for model_size in ["70m","410m", "1b", "2.8b", "6.9b", "12b"]:
             embeddings.append(embedding.mean(0)[token-2])
 
         names = [f"{i}" for i in range(continuation+1)]
-
-        # 创建一个空的 DataFrame 用于保存结果
-        plt.figure(figsize=(8, 6))
+        fig, axs = plt.subplots(2, 1, figsize=(10, 20))
         similarities = pd.DataFrame(index=names, columns=names)
         for i in range(len(embeddings)):
             for j in range(i, len(embeddings)):
                 similarity = F.cosine_similarity(embeddings[i].unsqueeze(0), embeddings[j].unsqueeze(0)).item()
                 similarities.loc[names[i], names[j]] = similarity
                 similarities.loc[names[j], names[i]] = similarity
-        plt.figure(figsize=(10, 10))
-        sns.heatmap(similarities.astype(float), annot=True, fmt=".3f", annot_kws={"size": 7}, square=True, cmap='hot',  xticklabels=True, yticklabels=True)
-        plt.title(f'Embedding Similarities_{token}')
-        plt.savefig(f'embedding_figure/embedding_similarities_{model_size}_{token}.png', bbox_inches='tight', pad_inches=0)
-        plt.show()
 
-        plt.figure(figsize=(8, 6))
+        sns.heatmap(similarities.astype(float), annot=True, fmt=".3f", annot_kws={"size": 7},
+                    square=True, cmap='hot', xticklabels=True, yticklabels=True, ax=axs[0])
+
+        axs[0].set_title(f'Embedding Similarities_{token}')
+
+        # Distances
         distances = pd.DataFrame(index=names, columns=names)
         for i in range(len(embeddings)):
             for j in range(i, len(embeddings)):
                 distance = torch.dist(embeddings[i].unsqueeze(0), embeddings[j].unsqueeze(0), p=2).item()
                 distances.loc[names[i], names[j]] = distance
                 distances.loc[names[j], names[i]] = distance
-        plt.figure(figsize=(10, 10))
+
         cmap = sns.diverging_palette(230, 20, as_cmap=True)
-        sns.heatmap(distances.astype(float), annot=True, fmt=".2f",  annot_kws={"size": 9}, square=True, cmap=cmap,  xticklabels=True, yticklabels=True)
-        plt.title(f'Embedding Distances_{token}')
-        plt.savefig(f'embedding_figure/embedding_distances_{model_size}_{token}.png', bbox_inches='tight', pad_inches=0)
+        sns.heatmap(distances.astype(float), annot=True, fmt=".2f", annot_kws={"size": 9},
+                    square=True, cmap=cmap, xticklabels=True, yticklabels=True, ax=axs[1])
+
+        axs[1].set_title(f'Embedding Distances_{token}')
+
+        plt.tight_layout()  # adjust subplot params so that subplots fit into the figure area
+        plt.savefig(f'embedding_figure/embedding_{model_size}_{token}.png', bbox_inches='tight', pad_inches=0)
         plt.show()
-        plt.figure(figsize=(8, 6))
+        # 创建一个空的 DataFrame 用于保存结果
+        # plt.figure(figsize=(10, 8))
+        # similarities = pd.DataFrame(index=names, columns=names)
+        # for i in range(len(embeddings)):
+        #     for j in range(i, len(embeddings)):
+        #         similarity = F.cosine_similarity(embeddings[i].unsqueeze(0), embeddings[j].unsqueeze(0)).item()
+        #         similarities.loc[names[i], names[j]] = similarity
+        #         similarities.loc[names[j], names[i]] = similarity
+        # plt.figure(figsize=(10, 10))
+        # sns.heatmap(similarities.astype(float), annot=True, fmt=".3f", annot_kws={"size": 7}, square=True, cmap='hot',  xticklabels=True, yticklabels=True)
+        # plt.title(f'Embedding Similarities_{token}')
+        # plt.savefig(f'embedding_figure/embedding_similarities_{model_size}_{token}.png', bbox_inches='tight', pad_inches=0)
+        # plt.show()
+        #
+        # plt.figure(figsize=(10, 8))
+        # distances = pd.DataFrame(index=names, columns=names)
+        # for i in range(len(embeddings)):
+        #     for j in range(i, len(embeddings)):
+        #         distance = torch.dist(embeddings[i].unsqueeze(0), embeddings[j].unsqueeze(0), p=2).item()
+        #         distances.loc[names[i], names[j]] = distance
+        #         distances.loc[names[j], names[i]] = distance
+        # plt.figure(figsize=(10, 10))
+        # cmap = sns.diverging_palette(230, 20, as_cmap=True)
+        # sns.heatmap(distances.astype(float), annot=True, fmt=".2f",  annot_kws={"size": 9}, square=True, cmap=cmap,  xticklabels=True, yticklabels=True)
+        # plt.title(f'Embedding Distances_{token}')
+        # plt.savefig(f'embedding_figure/embedding_distances_{model_size}_{token}.png', bbox_inches='tight', pad_inches=0)
+        # plt.show()
+
+
+        plt.figure(figsize=(10, 8))
         all_embeddings = np.stack([embedding.cpu().numpy() for embedding in embeddings], axis=0)
         n_samples = all_embeddings.shape[0]
         #pca = PCA(n_components=2, random_state=42)
