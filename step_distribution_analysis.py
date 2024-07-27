bd6ee2490f08e4ffc9414f62d570081470952e0f
PososAgapo <bchen@kth.se>
2024-07-27 17:17:03 +0900

    add index distribution analysis

--- step_distribution_analysis.py
+++ step_distribution_analysis.py
@@ -19,21 +19,30 @@ print(prefix)
 buff_size = 2049*1024*2
 print("Building dataset")
 mmap_ds = MMapIndexedDataset(prefix, skip_warmup=True)
+batch_size = 32
+df_small = pd.read_csv(f"generate_results/memorization_evals_{small_model_size}-deduped-v0_{context}_{context+continuation}_143000.csv", index_col=0)
 
-# df_small = pd.read_csv(f"generate_results/memorization_evals_{small_model_size}-deduped-v0_{context}_{context+continuation}_143000.csv", index_col=0)
-#
-# scores = [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
-# data_list = []
-# for score in scores:
-#     df_small_memorized = df_small[df_small["score"] == score]
-#     small_memorized_idx = df_small_memorized.index
-#     for idx in tqdm(small_memorized_idx[:1000]):
-#         data = mmap_ds[idx]
-#         data_list.append(data.tolist())
-# data = torch.tensor(data_list)
-# torch.save(data, "data_sample.pt")
+scores = [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1]
+data_list = []
+for score in scores:
+    df_small_memorized = df_small[df_small["score"] == score]
+    small_memorized_idx = df_small_memorized.index
+    for idx in tqdm(small_memorized_idx[:500]):
+        data = mmap_ds[idx]
+        data_list.append(data.tolist())
+data = torch.tensor(data_list)
+torch.save(data, "data_sample.pt")
+data = data.cpu().numpy() if data.is_cuda else data.numpy()
+df = pd.DataFrame(data)
+df.to_csv('data_sample.csv', index=False)
 
-data = torch.load("data_sample.pt")
+df = pd.read_csv('data_sample.csv')
+data_numpy = df.values
+
+data_tensor = torch.from_numpy(data_numpy)
+
+# 进行必要的类型转换（如果需要的话）
+data_tensor = data_tensor.int()
 
 model = GPTNeoXForCausalLM.from_pretrained(
     f"EleutherAI/pythia-{small_model_size}",
@@ -46,16 +55,33 @@ tokenizer = AutoTokenizer.from_pretrained(
   revision="step143000",
   cache_dir=f"./pythia-{small_model_size}-deduped/step143000",
 )
-accuracy_list = []
-for sample in data:
-    context_tokens = torch.tensor(sample[:context]).unsqueeze(0).cuda()
-    true_continuation = torch.tensor(sample[context:context + continuation]).unsqueeze(0).cuda()
+
+num_batches = len(data) // batch_size
+# Take care of the last batch if it doesn't align with the `batch_size`
+if len(data) % batch_size != 0:
+    num_batches += 1
+
+for i in range(num_batches):
+    # Calculate start and end index for each batch
+    start_idx = i * batch_size
+    end_idx = min((i + 1) * batch_size, len(data))
+
+    # Get the batch data
+    batch_data = data[start_idx:end_idx]
+
+    # Convert to PyTorch tensor and move to device
+    context_tokens = torch.tensor([sample[:context] for sample in batch_data]).cuda()
+    true_continuation = torch.tensor([sample[context:context + continuation] for sample in batch_data]).cuda()
+
     with torch.no_grad():
         generations = model.generate(context_tokens, temperature=0.0, top_k=0, top_p=0,
                                      max_length=context + continuation,
                                      min_length=context + continuation)
-        accuracies = (true_continuation == generations[:, context:context + continuation]).float().sum()
-        accuracy_list.append(accuracies.tolist())
+
+        # Calculate accuracy for this batch and store
+        accuracies = (true_continuation == generations[:, :, context:context + continuation]).float().sum(
+            dim=1).tolist()
+        accuracy_list.extend(accuracies)
 accuracy_list = torch.tensor(accuracy_list)
 for idx, score in enumerate(scores):
     temp = accuracy_list == score
